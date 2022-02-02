2d3417f0e2c68f9610e600782c8fcc228b3d2587
myles2021 <86742200+myles2021@users.noreply.github.com>
2022-02-02 11:33:00 +0000

    Update tf-main-variables-outputs.sh

--- tf-main-variables-outputs.sh
+++ tf-main-variables-outputs.sh
@@ -1,1 +1,42 @@
-gg
+aws configure
+mkdir tf-main-variables && cd $_
+code main.tf
+# provider "aws" {
+#     region                  = "eu-west-2"
+#     shared_credentials_file = "~/.aws/credentials"
+# }
+# resource "aws_instance" "EC2" {
+#     ami           = var.ami-id
+#     instance_type = var.instance-type
+#     key_name      = var.pem-key
+
+# }
+
+code variables.tf
+# variable "ami-id" {
+#     description = "AMI ID of ubuntu 20.04LTS eu-west-2"
+#     default     = "ami-0015a39e4b7c0966f"
+# }
+# variable "instance-type" {
+#     description = "Free tier EC2 Instance type"
+#     default     = "t2.micro"
+# }
+# variable "pem-key" {
+#     description = "Associated Key to SSH into the EC2 Instance"
+#     default     = "# NAME OF PEM KEY"
+# }
+
+code outputs.tf
+# output "ec2_id" {
+#     value = aws_instance.EC2.id
+# }
+# output "instance_public_ip" {
+#   description = "Public IP address of the EC2 instance"
+#   value       = aws_instance.EC2.public_ip
+# }
+
+terraform init
+terraform plan
+terraform apply
+terraform output
+terraform destroy
